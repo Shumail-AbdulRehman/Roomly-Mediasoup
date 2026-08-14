c10b08994569cfd3b0c2a179c037d014582e8445
Shumail Abdulrehman <47891@students.riphah.edu.pk>
2026-08-14 05:47:48 +0500

    feat(server): add WebSocket dispatcher and disconnect cleanup

--- server/src/lib/ws.ts
+++ server/src/lib/ws.ts
@@ -1,5 +1,5 @@
-import { WebSocketServer,WebSocket } from "ws";
-import { createRoom, joinRoom } from "./room.js";
+import { WebSocketServer, WebSocket } from "ws";
+import { createRoom, getRoom, joinRoom } from "./room.js";
 
 import onGetRouterRtpCapabilities from "../wsControllers/onGetRouterRtpCapabilities.js";
 import onCreateProducerTransport from "../wsControllers/onCreateProducerTransport.js";
@@ -8,67 +8,118 @@ import onConnectProducerTransport from "../wsControllers/onConnectProducerTransp
 import onProduce from "../wsControllers/onProduce.js";
 import onCreateConsumer from "../wsControllers/onCreateConsumer.js";
 import onResumeConsumer from "../wsControllers/onResumeConsumer.js";
+import onProducerClose from "../wsControllers/onProducerClose.js";
 
 import { Peer } from "../types/peer.types.js";
+import { onConnectConsumerTransport } from "../wsControllers/onConnectConsumerTransport.js";
+import onRequestExistingProducers from "../wsControllers/onRequestExistingProducers.js";
+import send from "../utils/send.js";
+import { rooms } from "../const/const.js";
 
-export const webSocketServer = (wss:WebSocketServer) => {
+export const webSocketServer = (wss: WebSocketServer) => {
+  wss.on("connection", (ws: WebSocket) => {
+    let peerId: string = crypto.randomUUID();
+    const peer: Peer = {
+      peerId,
+      ws,
+      transports: new Map(),
+      producers: new Map(),
+      consumers: new Map(),
+    };
+
+    ws.on("message", async (data: any) => {
+      let msg;
+try {
+         msg = JSON.parse(data);
   
-  wss.on("connection", (ws:WebSocket)=> {
-
-        let peerId:string=crypto.randomUUID();
-        const peer: Peer = {
-        peerId,
-        ws,
-        transports: new Map(),
-        producers: new Map(),
-        consumers: new Map(),
-      };
-
-      ws.on("message", async (data:any)=> {
-        const msg=JSON.parse(data);
-
-        if(msg.type === "createRoom") {
-          createRoom(peerId,peer);
-          
-        }
-
-        else if(msg.type === "joinRoom") {
-          joinRoom(peerId, msg.data.roomId,peer);
-          
-        }
-
-        else if(msg.type === "getRouterRtpCapabilities") {
-
-          onGetRouterRtpCapabilities(msg,peer);
-        }
-
-
-        else if(msg.type === "createProducerTransport") {
-          await onCreateProducerTransport(peer);
-        }
-
-        else if(msg.type === "createConsumerTransport") {
-          await onCreateConsumerTransport(msg,peer);
-        }
-
-        else if(msg.type === "connectProducerTransport") {
-
-          await onConnectProducerTransport(msg,peer);
-        }
-
-        else if(msg.type === "produce") {
-          await onProduce(msg,peer);
-        }
-
-        else if(msg.type === "createConsumer") {
-          await onCreateConsumer(msg,peer);
-        }
-
-        else if(msg.type === "resumeConsumer") {
-          onResumeConsumer(msg,peer);
-        }
-
-      })
-
-    })
+} catch (error) {
+    console.log("error while parsng JSON::",error);
+    return;
 }
+      if (msg.type === "createRoom") {
+        createRoom(peerId, peer);
+      } else if (msg.type === "joinRoom") {
+        joinRoom(peerId, msg.data.roomId, peer);
+      } else if (msg.type === "getRouterRtpCapabilities") {
+        onGetRouterRtpCapabilities(msg, peer);
+        console.log("rtp capa");
+      } else if (msg.type === "createProducerTransport") {
+        await onCreateProducerTransport(peer);
+      } else if (msg.type === "createConsumerTransport") {
+        await onCreateConsumerTransport(msg, peer);
+      } else if (msg.type === "connectProducerTransport") {
+        await onConnectProducerTransport(msg, peer);
+      } else if (msg.type === "connectConsumerTransport") {
+        await onConnectConsumerTransport(msg, peer);
+      } else if (msg.type === "produce") {
+        await onProduce(msg, peer);
+      } else if (msg.type === "createConsumer") {
+        await onCreateConsumer(msg, peer);
+      } else if (msg.type === "resumeConsumer") {
+        onResumeConsumer(msg, peer);
+      } else if (msg.type === "requestExistingProducers") {
+        onRequestExistingProducers(msg, peer);
+      } else if(msg.type === "producerClose") {
+        onProducerClose(msg,peer);
+      }
+    });
+
+    ws.on("close",()=> {
+
+      if(!peer.roomId) return;
+
+      const room=getRoom(peer.roomId);
+
+      if(!room) return;
+
+      try {
+        peer.producers.forEach((producer)=> {
+          room.peers.forEach((otherPeer)=> {
+            if(peer.peerId === otherPeer.peerId) return;
+  
+  
+            const msg={
+              type:"producerClosed",
+              data:{
+                producerId:producer.id
+              }
+            }
+  
+            send(otherPeer.ws,msg);
+  
+          })
+        })
+  
+  
+        peer.transports.forEach((transportData)=> {
+          transportData.transport.close();
+        });
+  
+  
+        peer.producers.forEach((producer)=> {
+          room.producers.delete(producer.id);
+        });
+  
+        peer.consumers.forEach((consumer)=> {
+          room.consumers.delete(consumer.id);
+        });
+  
+        room.peers.delete(peer.peerId);
+  
+        if(room.peers.size === 0){
+          room.router.close();
+          rooms.delete(peer.roomId);
+  
+        }
+  
+        peer.transports.clear();
+        peer.consumers.clear();
+        peer.producers.clear();
+        
+  
+      } catch (error) {
+        console.log("Error During Peer Cleanup::",error);
+      }
+    })
+  });
+};
