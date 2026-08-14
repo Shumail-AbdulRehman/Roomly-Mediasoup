eccc451cceaf1ccee5a9240cc99b0be1904bfb70
Shumail Abdulrehman <47891@students.riphah.edu.pk>
2026-08-14 05:47:48 +0500

    feat(client): add application entry with routing

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home.tsx";
+import Conference from "./pages/Conference.tsx";
+import { Provider } from "react-redux";
+import { store } from "./store/store.ts";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "home",
+        element: <Home />,
+      },
+      {
+        path: "conference",
+        element: <Conference />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>,
+);
